12842bcdbe5a6e0e92227bc9ae32876857216aa8
heehee <leejh080628@naver.com>
2024-09-02 16:18:47 +0900

    JS practice

---
+++ 1.js
@@ -0,0 +1,13 @@
+alert("hi");
+
+
+const userName = prompt(`이름을 입력하세요`, `아무개`);
+document.write(`당신의 이름은 <b>${userName}</b>입니다.`);
+console.log(`당신의 이름은 <b>%{userName}</b>입니다.`);
+
+console.log(3 + 3);
+
+const reply = confirm(`집에 가고 싶지?`);
+console.log(reply);
+
+const UareSmart = null;
---
+++ 2.js
@@ -0,0 +1,25 @@
+console.log(reply);
+
+const UareSmart = null;
+console.log(UareSmart);
+let amI;
+console.log(amI);
+
+const daysOfWeek = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
+console.log(daysOfWeek[2]);
+console.log(typeof daysOfWeek);
+
+const student = {
+    name: "chj",
+    grade: "A",
+    good: true,
+};
+
+console.log(student);
+console.log(student.name);
+for (const key in student) {
+    console.log(`${key}: ${student[key]}`);
+}
+
+console.log(parseInt("hello"));
+console.log(typeof NaN);
